9829162f022a7b029db62fe3f84876b1d365b5ed
shipeng <shipengpipi@126.com>
2014-11-19 21:53:05 +0800

    2014119_2153

--- jupin/vanstone-jupin-common/src/main/java/com/vanstone/jupin/common/JupinException.java
+++ jupin/vanstone-jupin-common/src/main/java/com/vanstone/jupin/common/JupinException.java
@@ -3,11 +3,13 @@
  */
 package com.vanstone.jupin.common;
 
+import com.vanstone.business.VanstoneException;
+
 /**
  * @author shipeng
  *
  */
-public class JupinException extends Exception {
+public class JupinException extends VanstoneException {
 		
 	private static final long serialVersionUID = 8642228317187421531L;
 	
