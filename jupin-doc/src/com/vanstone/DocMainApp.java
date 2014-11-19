db34952bcd2a212cc2a4c4b7e418452ff2a6ff89
soltex <shipengpipi@126.com>
2014-11-19 23:41:03 +0800

    20141119-2341

---
+++ jupin-doc/src/com/vanstone/DocMainApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.vanstone;
+
+/**
+ * @author shipeng
+ *
+ */
+public class DocMainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
